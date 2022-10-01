a2accaac17fa1c5081062794052805345cb69447
Martin Wimpress <code@flexion.org>
2022-10-02 00:39:09 +0100

    Automate building everything

--- build-auto.sh
+++ build-auto.sh
@@ -11,25 +11,28 @@ if [ -z "${1}" ] || [ -z "${2}" ]; then
 fi
 
 case "${1}" in
-    focal|jammy|kinetic) DISTRO="${1}";;
+    focal|jammy|kinetic) DISTROS="${1}";;
+    all) DISTROS="kinetic jammy focal";;
     *) echo "ERROR! Unknown Ubuntu release: ${1}"
       exit 1;;
 esac
 
 case "${2}" in
-    26|27|28) OBS_VERS="${2}";;
-    all)      OBS_VERS="28 27 26";;
+    26|27|28)  OBS_VERS="${2}";;
+    all) OBS_VERS="28 27 26";;
     *) echo "ERROR! Unsupported OBS Studio version: ${2}"
        exit 1;;
 esac
 
-for OBS_VER in ${OBS_VERS}; do
-    if [ "${DISTRO}" == "kinetic" ] && [ "${OBS_VER}" -le 27 ]; then
-        # Do not try and build old OBS versions on Ubuntu 22.10
-        continue
-    fi
-    ./build-trash.sh "${DISTRO}"
-    ./build-bootstrap.sh "${DISTRO}"
-    ./build-enter.sh "${DISTRO}" "/root/obs-portable.sh ${OBS_VER}"
-    ./build-get-artefacts.sh "${DISTRO}" "${OBS_VER}"
+for DISTRO in ${DISTROS}; do
+    for OBS_VER in ${OBS_VERS}; do
+        if [ "${DISTRO}" == "kinetic" ] && [ "${OBS_VER}" -le 27 ]; then
+            # Do not try and build old OBS versions on Ubuntu 22.10
+            continue
+        fi
+        ./build-trash.sh "${DISTRO}"
+        ./build-bootstrap.sh "${DISTRO}"
+        ./build-enter.sh "${DISTRO}" "/root/obs-portable.sh ${OBS_VER}"
+        ./build-get-artefacts.sh "${DISTRO}" "${OBS_VER}"
+    done
 done
