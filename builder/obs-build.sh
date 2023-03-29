789d29d6a62811039217389f35d0df8cdf77ac49
Martin Wimpress <code@flexion.org>
2023-03-29 18:22:15 +0100

    chore: code formatting

--- builder/obs-build.sh
+++ builder/obs-build.sh
@@ -525,10 +525,10 @@ function stage_07_plugins_out_tree() {
         # obs-midi-ng requires Qt 6 which is not available in Ubuntu 20.04
         if [ "${DISTRO_CMP_VER}" -le 2004 ]; then
             if [ "${PLUGIN}" == "obs-midi-mg" ] || \
-            [ "${PLUGIN}" == "obs-pipewire-audio-capture" ] || \
-            [ "${PLUGIN}" == "obs-teleport" ] || \
-            [ "${PLUGIN}" == "obs-vkcapture" ]; then
-                continue
+               [ "${PLUGIN}" == "obs-pipewire-audio-capture" ] || \
+               [ "${PLUGIN}" == "obs-teleport" ] || \
+               [ "${PLUGIN}" == "obs-vkcapture" ]; then
+                 continue
             fi
         fi
 
