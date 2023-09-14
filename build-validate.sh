26b0caf8d5549b072ef1e3ec76f6d79fffa749b0
Martin Wimpress <martin@wimpress.org>
2023-09-14 14:10:22 +0100

    fix: do not attempt to build OBS Studio 30+ on Ubuntu 20.04

--- build-validate.sh
+++ build-validate.sh
@@ -4,5 +4,10 @@ DISTRO="${1}"
 OBS_VER="${2}"
 OBS_MAJ_VER="${OBS_VER%%.*}"
 
+if [ "${DISTRO}" == "focal" ] && [ "${OBS_MAJ_VER}" -ge 30 ]; then
+    # OBS Studio 30+ is not support on Ubuntu 20.04
+    exit 1
+fi
+
 # Build is allowed to proceed
 exit 0
