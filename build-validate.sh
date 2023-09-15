285706d52362f7bab899d29e68c6036d4757c842
Martin Wimpress <martin@wimpress.org>
2023-09-15 09:46:38 +0100

    chore: print the build targets in build-validate.sh

--- build-validate.sh
+++ build-validate.sh
@@ -5,9 +5,10 @@ OBS_VER="${2}"
 OBS_MAJ_VER="${OBS_VER%%.*}"
 
 if [ "${DISTRO}" == "focal" ] && [ "${OBS_MAJ_VER}" -ge 30 ]; then
-    # OBS Studio 30+ is not support on Ubuntu 20.04
+    # OBS Studio 30+ is not supported on Ubuntu 20.04
     exit 1
 fi
 
 # Build is allowed to proceed
+echo "Building OBS ${OBS_VER} on ${DISTRO}"
 exit 0
